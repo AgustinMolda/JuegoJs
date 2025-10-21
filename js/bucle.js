e7e3d40227f66ccdad016dc61488fdc466ba77c7
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2025-10-20 23:13:30 -0300

    configurandoBotonDeStart

--- js/bucle.js
+++ js/bucle.js
@@ -5,16 +5,34 @@ let bucleInfinito={
     fps:0, //Frames por segundo
     bucle:function(tiempo){
         bucleInfinito.id=window.requestAnimationFrame(bucleInfinito.bucle);
+        
+        bucleInfinito.actualizar();
+        bucleInfinito.referscar();
+
         let diferencia= tiempo-bucleInfinito.fin;
         if(diferencia > 999){
             console.log(`
                 fin= ${bucleInfinito.fin}\n
                 tiempo= ${tiempo}
                 difrencia= ${diferencia}\n
+                aps= ${bucleInfinito.aps}\n
+                fps= ${bucleInfinito.fps}\n
+                id= ${bucleInfinito.id}\n
 
                 `);
             bucleInfinito.fin=tiempo;
+            bucleInfinito.fps=0;
+            bucleInfinito.aps=0;
         }
+    },
+
+    actualizar: function(){
+        bucleInfinito.aps++;
+
+    },
+
+    referscar: function(){
+        bucleInfinito.fps++;
     }
 
 }
--- js/evento.js
+++ js/evento.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("inicia juego");
+//     document.addEventListener('DOMContentLoaded', function(){
+//    console.log("inicia juego");
+//     bucleInfinito.bucle();
+//    });
+
+let start = document.getElementById('start');
+
+
+start.addEventListener('click', function(){
     bucleInfinito.bucle();
 });
